535c4ac8f88944b5b98ce5d6f3282c483d77d390
alperyyildiz <50810809+alperyyildiz@users.noreply.github.com>
2020-04-07 06:03:19 +0300

    Update MM.py

--- MM.py
+++ MM.py
@@ -591,7 +591,7 @@ def SET_EXPERIMENT(PARAMS_TO_CHANGE=None):
     print(P_OBJ.DICT)
     best = fmin(fn=P_OBJ.GET_MODEL,
                 space=P_OBJ.space,
-                algo=atpe.suggest,
+                algo=hyperopt.random.suggest,
                 max_evals=200)
     print('NEDEEEEEN')
 SET_EXPERIMENT()
